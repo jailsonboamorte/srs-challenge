5d643c64fc7e7f5703d9f449f13bd8aa49a12478
Jailson Boa Morte <jailsonboamorte@gmail.com>
2025-08-24 13:34:52 -0300

    Added tables

--- app/models/tables.py
+++ app/models/tables.py
@@ -40,9 +40,71 @@ class ModelCategoryEnum(enum.Enum):
     international = "international"
 
 
+class ModelStateEnum(enum.Enum):
+    AC = "Acre"
+    AL = "Alagoas"
+    AP = "Amapá"
+    AM = "Amazonas"
+    BA = "Bahia"
+    CE = "Ceará"
+    DF = "Distrito Federal"
+    ES = "Espírito Santo"
+    GO = "Goiás"
+    MA = "Maranhão"
+    MT = "Mato Grosso"
+    MS = "Mato Grosso do Sul"
+    MG = "Minas Gerais"
+    PA = "Pará"
+    PB = "Paraíba"
+    PR = "Paraná"
+    PE = "Pernambuco"
+    PI = "Piauí"
+    RJ = "Rio de Janeiro"
+    RN = "Rio Grande do Norte"
+    RS = "Rio Grande do Sul"
+    RO = "Rondônia"
+    RR = "Roraima"
+    SC = "Santa Catarina"
+    SP = "São Paulo"
+    SE = "Sergipe"
+    TO = "Tocantins"
+
+
+class ModelCityEnum(enum.Enum):
+    RIO_BRANCO = "Rio Branco"  # AC
+    MACEIO = "Maceió"  # AL
+    MACAPA = "Macapá"  # AP
+    MANAUS = "Manaus"  # AM
+    SALVADOR = "Salvador"  # BA
+    FORTALEZA = "Fortaleza"  # CE
+    BRASILIA = "Brasília"  # DF
+    VITORIA = "Vitória"  # ES
+    GOIANIA = "Goiânia"  # GO
+    SAO_LUIS = "São Luís"  # MA
+    CUIABA = "Cuiabá"  # MT
+    CAMPO_GRANDE = "Campo Grande"  # MS
+    BELO_HORIZONTE = "Belo Horizonte"  # MG
+    BELEM = "Belém"  # PA
+    JOAO_PESSOA = "João Pessoa"  # PB
+    CURITIBA = "Curitiba"  # PR
+    RECIFE = "Recife"  # PE
+    TERESINA = "Teresina"  # PI
+    RIO_DE_JANEIRO = "Rio de Janeiro"  # RJ
+    NATAL = "Natal"  # RN
+    PORTO_ALEGRE = "Porto Alegre"  # RS
+    PORTO_VELHO = "Porto Velho"  # RO
+    BOA_VISTA = "Boa Vista"  # RR
+    FLORIANOPOLIS = "Florianópolis"  # SC
+    SAO_PAULO = "São Paulo"  # SP
+    ARACAJU = "Aracaju"  # SE
+    PALMAS = "Palmas"  # TO
+
+
 class Producers(Base):
     __tablename__ = "producers"
-    category = Column(Enum(ModelCategoryEnum))
+    category = Column(
+        Enum(ModelCategoryEnum), nullable=False, server_default="national"
+    )
 
 
 class People(Base):
@@ -61,3 +123,68 @@ class Companies(Base):
     producer_id = Column(Integer, ForeignKey("producers.id"), nullable=False)
     fantasy_name = Column(String(100), nullable=False)
     cnpj = Column(String(20), nullable=False)
+
+
+class Addresses(Base):
+    __tablename__ = "addresses"
+
+    state = Column(Enum(ModelStateEnum), nullable=False)
+    city = Column(Enum(ModelCityEnum), nullable=False)
+    zip_code = Column(String(15), nullable=False)
+
+
+class Harvests(Base):
+    __tablename__ = "harvests"
+    __table_args__ = (UniqueConstraint("name", name="unique_harvest_name_idx"),)
+
+    name = Column(String(30), nullable=False)
+    description = Column(String(150), nullable=False)
+
+
+class AddressesProducers(Base):
+    __tablename__ = "addresses_producers"
+    __table_args__ = (
+        UniqueConstraint(
+            "address_id", "producer_id", name="unique_address_id_producer_id"
+        ),
+    )
+
+    address_id = Column(Integer, ForeignKey("addresses.id"), nullable=False)
+    producer_id = Column(Integer, ForeignKey("producers.id"), nullable=False)
+
+
+class Farms(Base):
+    __tablename__ = "farms"
+
+    address_id = Column(Integer, ForeignKey("addresses.id"), nullable=False)
+    name = Column(String(150), nullable=False)
+    total_area = Column(Integer, nullable=False, comment="Área total da fazenda")
+    arabel_area = Column(Integer, nullable=False, comment="Área agricultável")
+    vegetation_area = Column(Integer, nullable=False, comment="Área de vegetação")
+
+
+class FarmsProducers(Base):
+    __tablename__ = "farms_producers"
+    __table_args__ = (
+        UniqueConstraint(
+            "farm_id", "producer_id", name="unique_farm_id_producer_id_idx"
+        ),
+    )
+
+    farm_id = Column(Integer, ForeignKey("farms.id"), nullable=False)
+    producer_id = Column(Integer, ForeignKey("producers.id"), nullable=False)
+
+
+class Crops(Base):
+    __tablename__ = "crops"
+    __table_args__ = (
+        UniqueConstraint("farm_id", "harvest_id", name="unique_farm_id_harvest_id_idx"),
+    )
+
+    farm_id = Column(Integer, ForeignKey("farms.id"), nullable=False)
+    harvest_id = Column(Integer, ForeignKey("harvests.id"), nullable=False)
+    arabel_area = Column(Integer, nullable=False, comment="Área agricultável")
+    status = Column(Enum(ModelStatusEnum), nullable=False)
+
+
+# op.execute('DROP TYPE IF EXISTS modelcategoryenum;')
